3fab99cd9f3f51c8a39c8d775c0b1808b8c632ee
catlense <catlense@voronin.xyz>
2022-04-21 14:31:37 +0700

    Sale page init

---
+++ sale.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Акция!</title>
+
+  <link rel="stylesheet" href="/styles/common.css">
+  <link rel="stylesheet" href="/styles/page/index.css">
+</head>
+<body>
+  <?php include 'components/header/header.php' ?>
+  <div class="first-block">
+    <!-- <a href="#hot">Горячие предложения</a> -->
+    <div class="main-slider">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" style="background: url(/images/slider/1.jpg)">
+          <h1>ООО "ГПМ-Центр" - официальный дилер Zoomlion Heavy Industry Science & Technology Co., Ltd.</h1>
+          <a href="#about"><button>О компании</button></a>
+        </div>
+        <div class="swiper-slide" style="background: url(/images/slider/2.webp)">
+          <h1>Zoomlion heavy industry производит впечатление производимым ассортиментом спецтехники, это краны всех типов, землеройная техника - Экскаваторы и бульдозеры, буровая техника, дорожностроительная техника, бетонное оборудование всех типов, а так же все типы вилочных погрузчиков, штабелёров, ричтраков, погрузчиков для поддонов, буксировочных тягачей.</h1>
+          <a href="#about"><button>О компании</button></a>
+        </div>
+        <div class="swiper-slide" style="background: url(/images/slider/5.png)">
+          <h1>Zoomlion занесен в книгу рекордов Гиннесса как производитель:</h1>
+          <ul>
+            <li>
+              самого большого в мире гусеничного крана грузоподъёмностью 3200 тонн.
+            </li>
+            <li>
+              самого большого автомобильного крана грузоподъемностью 2500 тонн.
+            </li>
+            <li>
+              самого большого башенного крана грузоподъемностью 240 тонн.
+            </li>
+            <li>
+              самого большого башенного крана по длине стрелы 100 метров.
+            </li>
+            <li>
+              самого большого автомобильного бетононасоса с высотой подачи стрелы 101 метр.
+            </li>
+            <li>
+              самого большого в мире стационарного бетононасоса с максимальным давлением 400 бар.
+            </li>
+          </ul>
+        </div>
+        <div class="swiper-slide" style="background: url(https://www.gama.com.tr/wp-content/themes/welcome/assets/img/bg/trading.jpg) no-repeat;">
+          <h1> Благодаря высокому опыту сотрудников ГПМ-Центр, Вы получите грамотную техническую поддержку 7 дней в неделю.
+                        Вы можете быть уверены в надежности компании и выполнении всех гарантийных обязательств.</h1>
+        </div>
+        <div class="swiper-slide" style="background: url(https://sc02.alicdn.com/kf/H3fa6b2509578415ca78a4d3e1ac8df3aO/237371530/H3fa6b2509578415ca78a4d3e1ac8df3aO.jpg) no-repeat;">
+          <h1>Zoomlion производит технику по технологии немецкой компании JOST и LIEBHERR (Germany), а это значит, что Вы приобретаете краны европейского качества по доступным ценам и сможете эксплуатировать технику долгий срок, сэкономив на сервисе и комплектующих.</h1>
+        </div>
+        <div class="swiper-slide" style="background: url(https://1.bp.blogspot.com/-wwYnVxqJEIQ/VvJACi7tz7I/AAAAAAAAFL4/ljyVbwnLKFYdVE9WoI80s2k1lmjdzp8zw/s1600/Miya%2BForklift%2Bstock.JPG) no-repeat;">
+          <h1>
+            На вилочные погрузчики Zoomlion предоставляется расширенная гарантия:<br/>
+            18 месяцев или 3 000 моточасов.
+          </h1>
+        </div>
+        <div class="swiper-slide" style="background: url(https://stnews.ru/uploads/posts/2019-04/15547248121353660.jpg) no-repeat;">
+          <h1>
+          Экскаваторы Zoomlion 2022 года выпуска в наличии на складе в Москве, Санкт-Петербурге и Забайкальске, по самым интересным ценам.
+          </h1>
+        </div>
+      </div>
+      <div class="swiper-navigation">
+        <div class="main-slider-prev">
+          <img src="/images/left-arrow.png" alt="">
+        </div>
+        <div class="main-slider-next">
+          <img src="/images/right-arrow.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+  <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+  <script src="/js/index.js"></script>
+  <script src="/js/app.js"></script>
+</body>
+</html>
